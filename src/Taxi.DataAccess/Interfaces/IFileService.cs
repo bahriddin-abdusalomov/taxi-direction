7181650b4bfb43f0923682144d9da95fbd760bcb
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-07 02:23:09 +0500

    created filer service interface

---
+++ src/Taxi.DataAccess/Interfaces/IFileService.cs
@@ -0,0 +1,8 @@
+﻿namespace Taxi.DataAccess.Interfaces;
+
+public interface IFileService
+{
+    public void CreateFile(string fileName);
+    public void WriteDataToFile(string fileName, string data);
+    public int GetNumberOfLines(string fileName);
+}
