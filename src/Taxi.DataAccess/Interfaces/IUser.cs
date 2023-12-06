086e9e06905d388c6dbd61dbe17daacb9728d573
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-07 02:23:50 +0500

    created user interface

---
+++ src/Taxi.DataAccess/Interfaces/IUser.cs
@@ -0,0 +1,12 @@
+﻿using Taxi.Domain.Models.Users;
+
+namespace Taxi.DataAccess.Interfaces;
+
+public interface IUser
+{
+    public void CreateUser(User user, string fileName);
+    public string GetAllUser(string fileName);
+    public void UpdateUser(User user, string fileName);
+    public void DeleteUser(User user, string fileName);
+    public string GetUserById(int id, string fileName);
+}
