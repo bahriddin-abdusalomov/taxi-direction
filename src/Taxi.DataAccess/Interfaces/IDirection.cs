152fd6878f672334a6ad6a70c163aa9e26cf8fbd
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-07 02:21:57 +0500

    created direction interface

---
+++ src/Taxi.DataAccess/Interfaces/IDirection.cs
@@ -0,0 +1,13 @@
+﻿using Taxi.Domain.Models.Directions;
+using Taxi.Domain.Models.Users;
+
+namespace Taxi.DataAccess.Interfaces;
+
+public interface IDirection
+{
+    public void CreateDirection(Direction direction);
+    public string GetAllDirection();
+    public void UpdateDirection(Direction direction);
+    public void DeleteDirection(Direction direction);
+    public string GetDirectionById(int id);
+}
