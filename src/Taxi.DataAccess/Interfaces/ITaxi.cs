0b052ab6d3c81208cb34999a6c7f70f7591924b3
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-07 02:23:35 +0500

    created taxi interface

---
+++ src/Taxi.DataAccess/Interfaces/ITaxi.cs
@@ -0,0 +1,12 @@
+﻿using Taxi.Domain.Models.Transports;
+
+namespace Taxi.DataAccess.Interfaces;
+
+public interface ITaxi
+{
+    public void CreateTaxi(Car car);
+    public string GetAllTaxi();
+    public void UpdateTaxi(Car car);
+    public void DeleteTaxi(Car car);
+    public string GetTaxiById(int id);
+}
